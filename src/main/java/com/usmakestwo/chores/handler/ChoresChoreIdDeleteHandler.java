c220de3f92385dda0b3344f44fa978484cbd32f4
gonzalovazquez <gonzalovazquez010@gmail.com>
2017-12-24 20:07:52 -0500

    Added chore delete

--- src/main/java/com/usmakestwo/chores/handler/ChoresChoreIdDeleteHandler.java
+++ src/main/java/com/usmakestwo/chores/handler/ChoresChoreIdDeleteHandler.java
@@ -1,17 +1,108 @@
 
 package com.usmakestwo.chores.handler;
 
+/**
+ * Import networknt libraries
+ */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.security.JwtHelper;
+import com.networknt.config.Config;
+import com.networknt.status.Status;
+import com.networknt.service.SingletonServiceFactory;
+
+
+import com.usmakestwo.chores.model.Chore;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.util.HttpString;
-import java.util.HashMap;
+import io.undertow.util.Headers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
 public class ChoresChoreIdDeleteHandler implements HttpHandler {
+    // Set up logger
+    static final Logger logger = LoggerFactory.getLogger(ChoresChoreIdDeleteHandler.class);
+    static Map<String, Object> securityConfig = (Map)Config.getInstance().getJsonMapConfig(JwtHelper.SECURITY_CONFIG);
+    static boolean securityEnabled = (Boolean)securityConfig.get(JwtHelper.ENABLE_VERIFY_JWT);
+
+    // Access a configured DataSource;
+    private static final DataSource ds = (DataSource) SingletonServiceFactory.getBean(DataSource.class);
+
+    // Get a Jackson JSON Object Mapper, usuable for object serialization
+    private static final ObjectMapper mapper = Config.getInstance().getMapper();
+
+    /**
+     * Handle Request
+     * @param exchange
+     * @throws Exception
+     */
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        
-            exchange.endExchange();
-        
+
+        if (exchange.isInIoThread()) {
+            exchange.dispatch(this);
+            return;
+        }
+
+        Status status = null;
+        int statusCode = 200;
+        String resp = null;
+
+        // get chore id here.
+        Integer choreId = Integer.valueOf(exchange.getQueryParameters().get("id").getFirst());
+        Chore chore = null;
+
+        // Get data from SQL
+        try (final Connection connection = ds.getConnection()) {
+
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "DELETE FROM chore WHERE id = ?",
+                    ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)) {
+
+                statement.setInt(1, choreId);
+
+                try(ResultSet resultSet = statement.executeQuery()) {
+
+                    // extract chore data
+                    if (resultSet.next()) {
+                        System.out.println(resultSet);
+                        resp = mapper.writeValueAsString(resultSet);
+                    } else {
+                        // chore data not found
+                        status = new Status("ERR12013", choreId);
+                        statusCode = status.getStatusCode();
+
+                        // serialize the error response
+                        resp = mapper.writeValueAsString(status);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // log the exception
+            logger.error("Exception encountered in the chores API", e);
+
+            // This is a runtime exception
+            status = new Status("ERR10010");
+            statusCode = status.getStatusCode();
+
+            // serialize the error response
+            resp = mapper.writeValueAsString(status);
+        }
+
+        // set the content type in response
+        // exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "application/json");
+
+
+        // serialize the response object and set in the response
+        exchange.setStatusCode(statusCode);
+        exchange.getResponseSender().send(resp);
     }
 }
